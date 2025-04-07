67c7991b60816d3bc9f9a0dfc4841589c695a3c7
rasp <64428706+raspberry-hash@users.noreply.github.com>
2025-04-06 23:42:04 -0600

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -12,14 +12,14 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(process.env.token);
 
-const clientId = "1161478939630391378";
+const clientId = "1341641356027756634";
 
 (async () => {
   try {
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
-      Routes.applicationCommands("1161478939630391378"),
+      Routes.applicationCommands("1341641356027756634"),
       { body: commands },
     );
 
