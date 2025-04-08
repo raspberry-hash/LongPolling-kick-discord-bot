944ea8db2311362214e331e67bd59bd1f490f07d
rasp <64428706+raspberry-hash@users.noreply.github.com>
2025-04-07 22:52:29 -0600

    Update viewservers.js

--- commands/viewservers.js
+++ commands/viewservers.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
 const API_BASE = 'https://longpolling-kick-discord-bot-production.up.railway.app';
@@ -9,12 +9,7 @@ module.exports = {
     .setDescription('View a list of server UUIDs'),
 
   async execute(interaction) {
-    // Step 1: Fetch UUIDs from your backend
-    interaction.reply("Hello!")
-        
-    }
-
-    // Step 2: Create the list of UUIDs to display
-}
+    // Step 1: Just send the hello reply for now
+    interaction.reply("Hello!");
   }
-
+};
