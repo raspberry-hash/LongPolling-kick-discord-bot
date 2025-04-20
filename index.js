5d23176a5569d73c8478be9fa285e6883dc20705
rasp <64428706+raspberry-hash@users.noreply.github.com>
2025-04-20 13:32:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.get('/uuid-page', (req, res) => {
             headers: {
               'Content-Type': 'application/json'
             },
-            body: message
+            body: JSON.stringify({ message })
           })
           .then(response => response.text())
           .then(data => alert('Command Sent: ' + data))
